4b3ddcdcc0d924b60ff5c5a470c9f16b6dea0fa3
kayakid <121535812+kayakid@users.noreply.github.com>
2021-03-07 06:22:35 +0000

    fixed package.json

--- src/bin/coastline_manager.rs
+++ src/bin/coastline_manager.rs
@@ -66,3 +66,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let oanda_url = env::var("OANDA_URL")?;
     let oanda_account = env::var("OANDA_ACCOUNT")?;
+    let oanda_api_key = env::var("OANDA_API_KEY")?;
+
+    let client = Client::new(
+        oanda_url.clone(),
+        oanda_account.clone(),
+        oanda_api_key.clone(),
+    );
+    let mut hedger =
+        hedger_opt.unwrap_or_else(|| {
+            let mut inventory: AgentInventory<GearHedger> = AgentInventory::new();
+            //inventory.agents.insert(String::from("shortloser"), GearHedger::symmetric(1.0150, 1.0650, 0.0010, 0.0010, 422500.0));
+            inventory
+        });
+    //GearHedger::symmetric(1.0150, 1.0650, 0.0010, 422500.0));
+
+    let hedger_str = serde_json::to_string(&hedger).ok().unwrap();
+    println!("{}", hedger_str);
+
+    //let mut inventory: AgentInventory<GearHedger> = AgentInventory::new();
+    //inventory.agents.insert(String::from("shortloser"), hedger.clone());
+    // inventory.agents.insert(String::from("bias"), DriftingHedge::new(4.0000, 422500, 2.0000));
+
+    //let inv_str = serde_json::to_string(&inventory).ok().unwrap();
+    //println!("{}", inv_str);
+    let sp = spectrum_client.get().await.unwrap().to_spectrum();
+    let os = sp.overshoots.iter().filter(|p| p.0 == 0.1).last().unwrap().1;
+
+    let mut cDir = os.direction;
