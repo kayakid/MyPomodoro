6a9dc0b8f37afa019111104a6e4de332d1921220
kayakid <121535812+kayakid@users.noreply.github.com>
2021-03-14 17:40:27 +0000

    adding image

--- src/bin/trade_bicoast.rs
+++ src/bin/trade_bicoast.rs
@@ -4,3 +4,32 @@ extern crate gear_trading;
 use clap::{arg, command, Parser};
 use std::fs;
 use std::{thread, time};
+
+use chrono::DateTime;
+use chrono::Utc;
+use error_chain::error_chain;
+use serde::Deserialize;
+use serde_json::json;
+use std::env;
+//use reqwest::Client;
+use gear_trading::hff::account::*;
+use gear_trading::hff::agents::*;
+use gear_trading::hff::bicoastagent::*;
+use gear_trading::hff::quote::Tick;
+use gear_trading::oanda::client::Client;
+use gear_trading::oanda::*;
+
+use std::error::Error;
+use tokio::main;
+
+/*
+error_chain! {
+    foreign_links {
+        EnvVar(env::VarError);
+        HttpRequest(reqwest::Error);
+    }
+}
+*/
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
