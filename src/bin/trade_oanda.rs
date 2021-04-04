e1877c57cf30797f605009f497b490b270b5f826
kayakid <121535812+kayakid@users.noreply.github.com>
2021-04-04 10:17:35 +0000

    bindings

--- src/bin/trade_oanda.rs
+++ src/bin/trade_oanda.rs
@@ -17,3 +17,37 @@ use gear_trading::hff::agents::*;
 use gear_trading::hff::quote::Tick;
 use gear_trading::oanda::client::Client;
 use gear_trading::oanda::*;
+
+use std::error::Error;
+use tokio::main;
+
+/*
+error_chain! {
+    foreign_links {
+        EnvVar(env::VarError);
+        HttpRequest(reqwest::Error);
+    }
+}
+*/
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the hedger file
+    #[arg(short = 'f', long)]
+    hedger_file: Option<String>,
+
+    #[arg(short = 'a', long)]
+   agent: Option<String>,
+
+    #[arg(short = 'n', long)]
+   name: Option<String>,
+
+    #[clap(long)]
+    dry: bool,
+
+    #[clap(long)]
+    clean: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
