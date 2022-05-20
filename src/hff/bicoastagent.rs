39beb3e53e7439879582e23a059c981330cc2314
kayakid <121535812+kayakid@users.noreply.github.com>
2022-05-20 12:23:05 +0000

    Add link to demo

--- src/hff/bicoastagent.rs
+++ src/hff/bicoastagent.rs
@@ -172,4 +172,4 @@ mod tests {
         });
         agent.update_on_fill(&OrderFill {
             price: agent.gear_hedger.tentative_price,
-            units: 
+            units: agent.gear_hedger.tentative_exposur
