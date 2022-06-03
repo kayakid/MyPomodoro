77e1412ca4a3217dd91be93650048503f9bef8e9
kayakid <121535812+kayakid@users.noreply.github.com>
2022-06-03 07:58:51 +0000

    version 1.0.4

--- src/hff/bicoastagent.rs
+++ src/hff/bicoastagent.rs
@@ -182,4 +182,4 @@ mod tests {
         });
         assert_eq!(agent.gear_hedger.tentative_exposure - agent.exposure(), - agent.exposure());
         agent.update_on_fill(&OrderFill {
-            
+            price: agent.gear_hedger.tentati
