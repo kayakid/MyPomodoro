4d0279d6ac1b2f2105d7e90cb2103357800410c1
kayakid <121535812+kayakid@users.noreply.github.com>
2022-10-19 18:17:27 +0000

    added basic tests

--- src/hff/overshoot.rs
+++ src/hff/overshoot.rs
@@ -3,4 +3,6 @@ use serde::{Serialize,Deserialize};
 // classes from the spectrum service
 #[derive(Deserialize, Debug)]
 pub struct os {
-    pub instrument: S
+    pub instrument: String,
+    pub scale: f64,
+    
