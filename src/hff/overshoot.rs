3b5c746795ccc6bb43e8f03df577216818a8bc45
kayakid <121535812+kayakid@users.noreply.github.com>
2022-11-24 07:59:45 +0000

    SAK-10376 Replacing hard-coded number for servlet-api version with variable.

--- src/hff/overshoot.rs
+++ src/hff/overshoot.rs
@@ -116,4 +116,4 @@ impl Overshoot {
         100.0 * (self.peak - self.start) / self.start / self.scale
     }
     pub fn reversal(&self) -> f64 {
-        100.0 * (self.curren
+        100.0 * (self.current - self.peak) / self.peak / self.sca
