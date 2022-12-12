c1c2bd31b87f0b3ed847e7be663b33ab772e2c5e
kayakid <121535812+kayakid@users.noreply.github.com>
2022-12-12 20:35:51 +0000

    Fixed comment [skip CI]

--- src/hff/overshoot.rs
+++ src/hff/overshoot.rs
@@ -178,4 +178,5 @@ impl LocalLiquidity {
 
     pub fn update(&mut self, maxOS: f64) -> f64 {
         let os = maxOS.signum() * (maxOS.abs() - 1.0);
-       
+        let H1: f64 = 0.28;
+        le
