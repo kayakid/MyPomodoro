0038f015e39c667471dd3d16bbea0a0628d608d0
kayakid <121535812+kayakid@users.noreply.github.com>
2023-01-18 05:42:48 +0000

    Update npm-shrinkwrap.json

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,36 @@ pub struct Gear {
     // lower than p_0
     pub p_0: f64,
     pub g_0: f64,
+
+    // sorted asc price ranges and gears at extremities
+    pub g_i: Vec<GearRange>,
+
+    //higher then p_n
+    pub p_n: f64,
+    pub g_n: f64,
+
+}
+
+impl Gear {
+
+    pub fn positive(price0: f64, price1: f64) -> Self {
+        Self {
+            p_0: price0,
+            g_0: 1.0,
+            g_i: vec![GearRange{
+                p_start: price0,
+                g_start: 1.0,
+                p_end: price1,
+                g_end: 0.0,
+            }],
+            p_n: price1,
+            g_n: 0.0,
+        }
+    }
+
+    pub fn negative(price0: f64, price1: f64) -> Self {
+        Self {
+            p_0: price0,
+            g_0: 0.0,
+            g_i: vec![GearRange{
+                p_start: price0,
