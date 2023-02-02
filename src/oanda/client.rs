733e88b9d1129aad003f0b98acfe7bc2c150fea9
kayakid <121535812+kayakid@users.noreply.github.com>
2023-02-02 12:03:34 +0000

    Back to dev

--- src/oanda/client.rs
+++ src/oanda/client.rs
@@ -23,4 +23,5 @@ impl Client {
     pub async fn get_pricing(&self, instrument: String) -> Option<PricingResponse> {
         let request_url = format!("{}/v3/accounts/{}/pricing?instruments={}",self.url.clone(), self.account, instrument);
 
-        let response: Result<reqwest::Response, reqwest::Error> = self.cl
+        let response: Result<reqwest::Response, reqwest::Error> = self.client
+            .get(request_url)
