d85e5b4eb2e557bfd767efe3d26e1869e6708624
kayakid <121535812+kayakid@users.noreply.github.com>
2023-03-03 05:59:00 +0000

    Services squares

--- src/oanda/mod.rs
+++ src/oanda/mod.rs
@@ -16,3 +16,41 @@ pub struct SideResponse {
 pub struct PositionsResponse {
     instrument: String,
     long: SideResponse,
+    short: SideResponse,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct OpenPositionsResponse {
+    positions: Vec<PositionsResponse>,
+}
+
+impl OpenPositionsResponse {
+    pub fn to_position_vec(&self) -> Vec<Position> {
+        let mut ret = Vec::new();
+        for opr in self.positions.iter() {
+            let units = if opr.long.units != "0"
+            { opr.long.units.parse().unwrap()}
+            else {  opr.short.units.parse().unwrap() };
+
+            let price = if opr.long.units != "0"
+            { opr.long.averagePrice.as_ref().map(|p| p.parse().unwrap())}
+            else { opr.short.averagePrice.as_ref().map(|p| p.parse().unwrap()) };
+
+            let position = Position {
+                instrument: opr.instrument.clone(),
+                units: units,
+                price: price
+            };
+            ret.push(position);
+        }
+        ret
+    }
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct LiquidityResponse {
+    price: String,
+    liquidity: i64,
+}
+#[derive(Deserialize, Debug)]
